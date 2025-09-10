a92a7e3a8fa97d950bdaa74424fc786a1977a04d
nakuwa23 <b.nakuwa@alustudent.com>
2025-09-10 13:50:21 -0700

    Script 1

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Regular expression must matches school, this script is written by Latjor
+# Regular expression must matches school
 puts ARGV[0].scan(/School/).join
 
---
+++ regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# 1. Repetition Token #0
+puts ARGV[0].scan(/hbt{2,5}n/).join
+
